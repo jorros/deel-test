e4ee2f558a23b4e1cd039ab89a8f2ef6ae18b72f
Jörg Rosenauer <joergrosenauer@icloud.com>
2022-11-04 14:46:50 +0100

    add contracts endpoint

--- src/routes/contracts.route.js
+++ src/routes/contracts.route.js
@@ -1,16 +1,22 @@
 const {getProfile} = require("../middleware/getProfile.middleware");
-const {getContractById} = require("../services/contract.service");
+const {getContractById, getContracts} = require("../services/contract.service");
 
 const contracts = (app) => {
     app.get('/contracts/:id', getProfile, async (req, res) => {
-        const {id} = req.params
+        const {id} = req.params;
         const contract = await getContractById(id, req.profile.id);
 
         if (!contract) {
-            return res.status(404).end()
+            return res.status(404).end();
         }
 
-        res.json(contract)
+        res.json(contract);
+    });
+
+    app.get('/contracts', getProfile, async(req, res) => {
+        const contracts = await getContracts(req.profile.id);
+
+        res.json(contracts);
     })
 }
 
--- src/services/contract.service.js
+++ src/services/contract.service.js
@@ -6,7 +6,7 @@ const {Op} = require("sequelize");
  *
  * @param {Number} id - The contract id
  * @param {Number} profileId - The profile requesting this contract
- * @returns {Contract} A Contract entity
+ * @returns {Promise.<Contract>} A Contract entity
  */
 const getContractById = async (id, profileId) => {
     const contract = await Contract.findOne({
@@ -21,9 +21,35 @@ const getContractById = async (id, profileId) => {
                 }
             ]
         }
-    })
+    });
 
     return contract;
 }
 
-module.exports = {getContractById};
+/**
+ * Queries the database to fetch all non terminated contracts connected to the profile
+ *
+ * @param {Number} profileId - The profile requesting the contracts
+ * @returns {Promise.<Contract[]>} All contracts
+ */
+const getContracts = async (profileId) => {
+    const contracts = await Contract.findAll({
+        where: {
+            status: {
+                [Op.ne]: 'terminated'
+            },
+            [Op.or]: [
+                {
+                    ClientId: profileId
+                },
+                {
+                    ContractorId: profileId
+                }
+            ]
+        }
+    });
+
+    return contracts;
+}
+
+module.exports = {getContractById, getContracts};
